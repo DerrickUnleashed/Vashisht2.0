c014d84f5dc3fe98a25c66b650bcf83a64f28aa5
DerrickUnleashed <derrickrds@gmail.com>
2025-03-30 15:26:49 +0530

    Deleting useless stuff...

--- Backend/test.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-pipe = pipeline("fill-mask", model="./local_bert", tokenizer="./local_bert")
-sentence = "the weather is"
-result = pipe(sentence + " [MASK]")
-for i in result:
-    if i['token_str'].isalpha():
-        print(i['token_str'])
